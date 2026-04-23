cee0ba524898a3103a3b827fe049445fddee60d8
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-24 06:54:07 +0900

    crt.h ヘッダーを追加し、CRT 抽象 API の標準入口を整備。README.md を更新して新しいヘッダーの使用を明示。

---
+++ prod/include/com_util/crt/crt.h
@@ -0,0 +1,28 @@
+/**
+ *******************************************************************************
+ *  @file           crt.h
+ *  @brief          CRT 抽象 API の集約ヘッダー。
+ *  @author         Tetsuo Honda
+ *  @date           2026/04/24
+ *
+ *  @details
+ *  `com_util/crt` 配下の公開ヘッダーをまとめて取り込む標準入口です。\n
+ *  CRT 抽象 API を広く利用する場合は、本ヘッダーの利用を推奨します。
+ *
+ *  @copyright      Copyright (C) Tetsuo Honda. 2026. All rights reserved.
+ *
+ *******************************************************************************
+ */
+
+#ifndef COM_UTIL_CRT_CRT_H
+#define COM_UTIL_CRT_CRT_H
+
+#include <com_util/crt/path.h>
+#include <com_util/crt/fcntl.h>
+#include <com_util/crt/stdio.h>
+#include <com_util/crt/string.h>
+#include <com_util/crt/time.h>
+#include <com_util/crt/unistd.h>
+#include <com_util/crt/sys/stat.h>
+
+#endif /* COM_UTIL_CRT_CRT_H */
