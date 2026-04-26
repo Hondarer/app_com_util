87fe7ad0e05630481f03e000ebe138dae78d7d3d
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-26 14:46:09 +0900

    テストソースにETWバックエンドを追加し、プラットフォーム依存の条件分岐を整理

--- test/src/integration/trace/traceFileIntegrationTest/makepart.mk
+++ test/src/integration/trace/traceFileIntegrationTest/makepart.mk
@@ -5,17 +5,9 @@ TEST_SRCS := \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt/time.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt/file.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt/stdio.c \
-    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/file/trace_file.c
-
-ifdef PLATFORM_LINUX
-TEST_SRCS += \
-    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/syslog/trace_syslog.c
-endif
-
-ifdef PLATFORM_WINDOWS
-TEST_SRCS += \
+    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/file/trace_file.c \
+    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/syslog/trace_syslog.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw.c
-endif
 
 # TEST_SRCS の相対インクルード解決
 INCDIR += \
